4e5db8d11c15d1df5b0d0253aa7db810f120ae10
Anthony <ptonyptc@gmail.com>
2019-08-26 10:47:38 -0500

    using let blocks and readme file created

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,30 +3,32 @@
 require './lib/tic_tac_toe.rb'
 
 RSpec.describe Board do
+
+  let(:board) { board = Board.new }
+
   describe ' #initialize_Board ' do
+    
     it ' test the correct number on each cell of the grid' do
-      board = Board.new
       cond = true
       m_board = board.main_board.flatten
       m_board.each_with_index do |cell, index|
-        cond = cell.to_i == (index + 1)
-        break unless cond
+          cond = cell.to_i == (index + 1)
+          break unless cond
       end
       expect(cond).to be true
     end
   end
 
   describe '#check space' do
+
     it 'returns true when a space is availabe in the board' do
-      board = Board.new
-      cond = board.space?
-      expect(cond).to be true
+        cond = board.space?
+        expect(cond).to be true
     end
 
     it 'returns false when a space is not availabe in the board' do
-      board = Board.new
       9.times do |n|
-        board.save_choise_player('X', n + 1)
+        board.save_choise_player('X',n + 1)
       end
       cond = board.space?
       expect(cond).to be false
@@ -34,17 +36,16 @@ RSpec.describe Board do
   end
 
   describe ' #check_choise_player' do
+
     it ' when a player select a number of the empty grid ' do
-      board = Board.new
       cond = board.check_choise_player(1) && board.check_choise_player(2)
       expect(cond).to be true
     end
 
     it ' when a player select a number of the grid with saves' do
-      board = Board.new
-      board.main_board[0][0] = 'X'
-      board.main_board[0][1] = 'O'
-      board.main_board[0][2] = 'X'
+      board.main_board[0][0] = 'X' 
+      board.main_board[0][1] = 'O' 
+      board.main_board[0][2] = 'X' 
 
       cond = board.check_choise_player(1) &&
              board.check_choise_player(2) &&
@@ -55,9 +56,8 @@ RSpec.describe Board do
   end
 
   describe ' #save_choise_player ' do
-    it ' when a mark is saved in the grid of each Player selection' do
-      board = Board.new
 
+    it ' when a mark is saved in the grid of each Player selection' do
       board.save_choise_player('X', 1)
       board.save_choise_player('O', 2)
       cond = board.main_board[0][1] == 'O' && board.main_board[0][0] == 'X'
--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -3,23 +3,26 @@
 require './lib/tic_tac_toe.rb'
 
 RSpec.describe TicTacToe do
+
+  let(:game) { TicTacToe.new('X') }
+
   describe ' #initialize_Tic_tac_toe ' do
-    it ' save the correct MARK in each player if player1 is X' do
-      game = TicTacToe.new('X')
+
+    it ' creating a game with arg X, return if the marks are correct' do
       cond = game.player1.identity == 'X' && game.player2.identity == 'O' ? true : false
       expect(cond).to be true
     end
 
-    it ' save the correct MARK in each player if player1 is O' do
-      game = TicTacToe.new('O')
-      cond = game.player1.identity == 'O' && game.player2.identity == 'X' ? true : false
-      expect(cond).to be true
+    it ' creating a game with arg X return if the marks are incorrect' do
+      cond = game.player1.identity == 'O' || game.player2.identity == 'X' ? true : false
+      expect(cond).to be false
     end
+
   end
 
   describe '#check_winnner' do
+
     it 'player is the winner' do
-      game = TicTacToe.new('O')
       game.board.save_choise_player(game.player1.identity, 1)
       game.board.save_choise_player(game.player1.identity, 2)
       game.board.save_choise_player(game.player1.identity, 3)
@@ -27,9 +30,10 @@ RSpec.describe TicTacToe do
       if game.board.main_board[0][0] == game.board.main_board[0][1] &&
          game.board.main_board[0][1] == game.board.main_board[0][2] &&
          game.board.main_board[0][2] == game.player1.identity
-        cond = true
+          cond = true
       end
       expect(cond).to be true
     end
+
   end
 end
